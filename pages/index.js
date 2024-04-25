ff7943b1b67ef6a0ee640865be6a777dcc429ebd
Coleman Gariety <garietyc@reed.edu>
2024-04-24 19:32:27 -0700

    not hysterical

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       <p>For example, look at the lyrics of The Beatles 'Strawberry Fields Forever': "Nothing is real, it doesn't matter much to me, living is easy with eyes closed, take me down to that endless red place la la la." This is nihilism: it says, "hell isn't so bad, come try it out!" It's pure evil.</p>
       <p>If you've been listening to this music, cure yourself by listening to Gregorian and Orthodox chants instead:</p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/i-3h9TQ312c?si=uP8QjsUtsSotiSs6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <p>By the way I'm shutting down topsters. I should've shut it down long ago. The music industry is evil.</p>
+      <p>By the way I'm shutting down topsters.</p>
       </main>
 
       <Footer />
